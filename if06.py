d52cad27954f2a2906c74222775dafe1110b6fa5
sobirismatov <name@uzcoder96@gmail.com>
2022-10-31 11:57:04 +0500

    natija

--- if06.py
+++ if06.py
@@ -19,13 +19,13 @@ def main(n):
     if(mx < n2):
         mx = n2
         index = 2
-    elif(mx < n3):
+    if(mx < n3):
         mx = n3
         index = 3
-    elif(mx < n4):
+    if(mx < n4):
         mx = n4
         index = 4
-    elif(mx < n5):
+    if(mx < n5):
         mx = n5
         index = 5
     else:
