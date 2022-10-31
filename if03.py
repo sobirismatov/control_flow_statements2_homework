cd7b4ef0570d4a4b1659390508d962ba9a26a7e4
sobirismatov <name@uzcoder96@gmail.com>
2022-10-31 15:41:16 +0500

    natija

--- if03.py
+++ if03.py
@@ -11,10 +11,9 @@ def main(a,b,c):
     d=b
     if a<b and b<c or c<b and b<a:
         d=b
-    elif a<c and c<b or b<c and c<a:
+    if a<c and c<b or b<c and c<a:
         d=c
-    elif b<a and a<c or c<a and a<b:
+    if b<a and a<c or c<a and a<b:
         d=a
         
     return d
-print(main(4,5,2))
